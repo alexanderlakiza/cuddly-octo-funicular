e8d1ac1f83ff97c2b410f6884ffa2d121b5d383e
Alexander Lakiza <alelakiza@yandex.ru>
2022-12-23 02:19:14 +0300

    Update recsys.py

--- course_work/my_rec_sys/recsys.py
+++ course_work/my_rec_sys/recsys.py
@@ -4,8 +4,8 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 
-df = pd.read_csv('data/movies_df.csv')
-# df = pd.read_csv('../data/movies_df.csv')
+# df = pd.read_csv('data/movies_df.csv')
+df = pd.read_csv('../data/movies_df.csv')
 df['overview'] = df['overview'].fillna('')
 indices = pd.Series(df.index, index=df['title'])
 
