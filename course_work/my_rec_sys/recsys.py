b9adfeaa8d5111a20d0d7c536499e8c59393bdda
Alexander Lakiza <alelakiza@yandex.ru>
2022-12-23 02:14:31 +0300

    Build the very first version of recsys application

---
+++ course_work/my_rec_sys/recsys.py
@@ -0,0 +1,65 @@
+from collections import Counter
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+
+df = pd.read_csv('data/movies_df.csv')
+# df = pd.read_csv('../data/movies_df.csv')
+df['overview'] = df['overview'].fillna('')
+indices = pd.Series(df.index, index=df['title'])
+
+
+def check_movie_appearance(movie):
+    return movie in df['title'].unique().tolist()
+
+
+class RecommendationSystem:
+    def __init__(self):
+        self.plot_matrix = self.make_tfidf_matrix()
+        self.metadata_matrix = self.make_countvec_matrix()
+
+    def get_recs(self, movies, show_values=False):
+        results_dict = {}
+        for movie in movies:
+            if movie:
+                for matrix in [self.plot_matrix, self.metadata_matrix]:
+                    results_dict = dict(Counter(results_dict) +
+                                        Counter(self.count_top_10(movie, matrix)))
+        results_dict = {k: v for k, v in sorted(results_dict.items(),
+                                                key=lambda item: item[1], reverse=True)}
+        results_dict = dict(list(results_dict.items())[:10])
+        if show_values:
+            return results_dict
+        return list(results_dict.keys())
+
+    def count_top_10(self, title, matrix):
+        idx = indices[title]
+        sim_scores = list(enumerate(matrix[idx]))
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1:11]
+        sim_scores = [(df['title'].iloc[i[0]], i[1]) for i in sim_scores]
+        return dict(sim_scores)
+
+    @staticmethod
+    def min_max_scaler(array):
+        return [(i - min(array)) / (max(array) - min(array))
+                for i in array]
+
+    @staticmethod
+    def make_tfidf_matrix():
+        tfidf = TfidfVectorizer(stop_words='english')
+        tfidf_matrix = tfidf.fit_transform(df['overview'])
+        return linear_kernel(tfidf_matrix, tfidf_matrix)
+
+    @staticmethod
+    def make_countvec_matrix():
+        count = CountVectorizer(stop_words='english')
+        count_matrix = count.fit_transform(df['soup'])
+        return cosine_similarity(count_matrix, count_matrix)
+
+
+if __name__ == "__main__":
+    movies = ['The Avengers', "Spectre", "Toy Story 3"]
+    rec = RecommendationSystem()
+    print(rec.get_recs(movies))
---
+++ course_work/streamlit_app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+
+from my_rec_sys.recsys import check_movie_appearance, RecommendationSystem
+
+
+@st.cache(ttl=100000)
+def get_recs(movies):
+    rec_sys = RecommendationSystem()
+    return rec_sys.get_recs(movies)
+
+
+st.title("Рекомендательная система")
+st.markdown("## Как устроена рекомендательная система")
+st.markdown("Введите один, два или три фильма в "
+            "поля ниже. К сожалению, база фильмов небольшая - всего 4.8 тысячи фильмов "
+            "(В основном, фильмы 21 века, выпущенные до начала 2017 года). "
+            "Названия фильмов необходимо вводить по-английски. Если вы не знаете название фильма по-английски, то"
+            " вы можете найти свой фильм на [Кинопоиске](https://www.kinopoisk.ru) и взять английское название "
+            "фильма оттуда (они находятся под российскими названиями фильмов). Или вы можете посмотреть в "
+            "[эксель "
+            "таблице](https://docs.google.com/spreadsheets/d"
+            "/1o939ac04UdB6rcXJH6eB0htRyzBi6icnAlkt2WoumOk/edit?usp=sharing), какие фильмы есть в базе. "
+            "После того, как вы введёте названия фильмов, необходимо нажать на кнопку, и тогда сервис предложит "
+            "вам 10 фильмов, которые больше всего похожи на те, что вы указали")
+
+with st.form("Watched Movies"):
+    st.write("Введите от 1 до 3 своих любимых фильма")
+    movie1 = st.text_input("Первый фильм")
+    movie2 = st.text_input("Второй фильм")
+    movie3 = st.text_input("Третий фильм")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Узнать рекомендации")
+
+    st.write('Попробуйте, например, ввести: The Avengers, Spectre, Toy Story 3')
+
+if submitted:
+    movies = [movie1, movie2, movie3]
+    for movie in movies:
+        if movie and not check_movie_appearance(movie1):
+            st.error(f'Фильм "__{movie}__" не был найден в нашей базе. Пожалуйста, убедитесь, что вы '
+                     f'правильно ввели английское название фильма. Если вы уверены, что ввели его правильно, '
+                     f'значит фильма нет в нашей базе, выберите другой фильм или оставьте поле пустым')
+            break
+    else:
+        st.markdown("---")
+        st.markdown("## Рекомендации")
+        recs_10 = get_recs(movies)
+        for rec_movie in recs_10:
+            st.success(f'Вам следует посмотреть __"{rec_movie}"__')
